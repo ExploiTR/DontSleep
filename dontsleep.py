685dc51caf2242e54616c9eb04712c8ffd86de1c
Pratim Majumder <ExploiTR@users.noreply.github.com>
2023-05-15 08:54:53 +0530

    Update dontsleep.py
    +No need for icon file, generated using PIL
    +Add option for mouse simulation (for games)
    +fix cmd window launch bug

--- dontsleep.py
+++ dontsleep.py
@@ -3,8 +3,9 @@ import time
 import ctypes
 import argparse
 import pystray
+import pyautogui
 import pygetwindow as gw
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 # Define Windows constants
 ES_CONTINUOUS = 0x80000000
@@ -18,14 +19,34 @@ def prevent_sleep():
 def restore_sleep():
     ctypes.windll.kernel32.SetThreadExecutionState(ES_CONTINUOUS)
 
+def simulate_mouse():
+    while True:
+        pyautogui.move(100, 0, duration=0.25)  
+        pyautogui.move(0, 100, duration=0.25) 
+        pyautogui.move(-100, 0, duration=0.25) 
+        pyautogui.move(0, -100, duration=0.25) 
+        time.sleep(10) # do this 4s every minute
+
 def exit_action(icon, item):
     icon.stop()
     restore_sleep()
     sys.exit()
 
+def create_image():
+    # Create a new image with RGB mode, size 64x64 and blue color
+    img = Image.new('RGB', (64, 64), color = 'blue')
+
+    d = ImageDraw.Draw(img)
+    fnt = ImageFont.truetype('arial.ttf', 30) # You may need to specify the full path of the font file
+
+    # Draw text in the center
+    d.text((10,10), "DS", font=fnt, fill=(255, 255, 255))
+
+    return img
+
 def create_tray_icon():
-    # Load an icon image
-    image = Image.open("icon.png")
+    # Create an icon image
+    image = create_image()
     # Create the tray icon
     icon = pystray.Icon("prevent_sleep", image, "Prevent Sleep", menu=pystray.Menu(pystray.MenuItem("Exit", exit_action)))
     icon.run()
@@ -34,6 +55,7 @@ def main():
     # Parse command line arguments
     parser = argparse.ArgumentParser(description="Prevent system sleep.")
     parser.add_argument('-v', '--verbose', action='store_true', help="Show CMD output window")
+    parser.add_argument('-m', '--mouse', action='store_true', help="Use mouse movement to prevent sleep")
     args = parser.parse_args()
 
     # Hide the CMD output window if -v flag is not passed
@@ -42,7 +64,11 @@ def main():
         if cmd_window:
             cmd_window.hide()
 
-    prevent_sleep()
+    if args.mouse:
+        simulate_mouse()
+    else:
+        prevent_sleep()
+
     create_tray_icon()
 
 if __name__ == "__main__":
